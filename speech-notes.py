a5a3af188f7a162d9472530befaaf6e8ce4117f5
Konstantin Itskov <konstantin.itskov@kovits.com>
2015-01-17 23:50:08 -0500

    This is likely to be the last commit because this entire project is not working.

--- speech-notes.py
+++ speech-notes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import alsaaudio
+import time
 import wave
 import numpy
 import threading
@@ -7,27 +8,40 @@ import sys
 import subprocess
 
 def timed_loop():
-    threading.Timer(5.0, timed_loop).start()
+    global event, w
+    event.clear()
+    threading.Timer(2.0, timed_loop).start()
+    subprocess.call(["ffmpeg", "-loglevel", "panic", "-i", filename_wav, "-y", "-vn", "-acodec", "libfaac", filename_mp4])
     w.close()
-    subprocess.call(["ffmpeg", "-i", filename_wav, "-y", "-vn", "-acodec", "libfaac", filename_mp4])
-    i = (i + 1) % 10
-    w = wave.open(filename, 'w')
+    subprocess.call(['curl -X POST --form "file=@output.mp4" --form "apikey=e5ca0d01-0fb2-4dc1-9ed7-20ee8873721f" https://api.idolondemand.com/1/api/sync/recognizespeech/v1'], shell = True)
+    print
+    # data = urllib.urlencode({'file': '@output.mp4', 'apikey':'e5ca0d01-0fb2-4dc1-9ed7-20ee8873721f'})
+    # r = urllib2.urlopen('https://api.idolondemand.com/1/api/sync/recognizespeech/v1', data)
+    # print r.read()
+    # sys.stdout.flush()
+    w = wave.open(filename_wav, "w")
+    w.setnchannels(1)
+    w.setsampwidth(2)
+    w.setframerate(44100)
+    event.set()
 
 inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE)
 inp.setchannels(1)
 inp.setrate(44100)
 inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
 inp.setperiodsize(1024)
+event = threading.Event()
+event.set()
 
-i = 0
-filename_wav = 'output' + i + '.wav'
-filename_mp4 = 'output' + i + '.mp4'
-w = wave.open(filename, 'w')
+filename_wav = 'output.wav'
+filename_mp4 = 'output.mp4'
+w = wave.open(filename_wav, 'w')
 w.setnchannels(1)
 w.setsampwidth(2)
 w.setframerate(44100)
 
-timed_loop()
+threading.Timer(2.0, timed_loop).start()
 while True:
+    event.wait()
     l, data = inp.read()
     w.writeframes(data)
